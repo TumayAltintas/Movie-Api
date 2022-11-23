40e2c49e922825fcb5725456c36c378a64d317b4
TumayAltintas <100495086+TumayAltintas@users.noreply.github.com>
2022-11-23 23:21:16 +0300

    BotHeader Add

--- js/script.js
+++ js/script.js
@@ -1,32 +1,77 @@
 const API_KEY = 'api_key=0fd2eb610862a35172254f63379f6e14';
 const TOP_TV_SERIES_URL = 'https://api.themoviedb.org/3/tv/top_rated?';
-const API_URL_TV = TOP_TV_SERIES_URL + API_KEY + '&language=en-US&page=1';
+const API_FOR_TV = TOP_TV_SERIES_URL + API_KEY + '&language=en-US&page=1';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const TOP_MOVIE_URL = 'https://api.themoviedb.org/3/movie/top_rated?'
 const API_FOR_MOVIES = TOP_MOVIE_URL + API_KEY + '&language=en-US&page=1';
 const main = document.getElementById('main');
 const second = document.getElementById('second');
-
+const HeaderTv = document.getElementById('HeaderTv');
+const HeaderMovie = document.getElementById('HeaderMovie');
 
 
 let a = 0
 let b = 5
+// Header Start
+GetTopTvHeaderSeries(API_FOR_TV);
 
+function GetTopTvHeaderSeries(url) {
+	let RandomNumber = Math.floor(Math.random() * 10);
+	console.log(RandomNumber)
+	fetch(url).then(res => res.json()).then(data => {
+		return ShowTopTvHeaderSeries(data.results.slice(1, 2));
+	})
+}
 
+function ShowTopTvHeaderSeries(data) {
+	HeaderTv.innerHTML = '';
+	data.forEach(movie => {
+		const {poster_path, vote_average} = movie
+		const movieEl = document.createElement('div');
+		movieEl.classList.add('MovieImage');
+		movieEl.innerHTML = `
+				<div class="HeaderShowMovie">
+				<h3 style="color:white">${vote_average}/10</h3>	
+				</div>	
+				<img class="TvImg" src="${IMG_URL + poster_path}">`
+		HeaderTv.appendChild(movieEl);
+	})
+}
 
+///
+GetTopMovieHeader(API_FOR_MOVIES);
+
+function GetTopMovieHeader(url) {
+	fetch(url).then(res => res.json()).then(data => {
+		return ShowTopMovieHeader(data.results.slice(14, 15));
+	})
+}
+
+function ShowTopMovieHeader(data) {
+	HeaderMovie.innerHTML = '';
+	data.forEach(movie => {
+		const {poster_path, vote_average} = movie
+		const movieEl = document.createElement('div');
+		movieEl.classList.add('MovieImage');
+		movieEl.innerHTML = `
+				<div class="HeaderShowMovie">
+				<h3 style="color:white">${vote_average}/10</h3>	
+				</div>				
+				<img class="TvImg" src="${IMG_URL + poster_path}">`
+		HeaderMovie.appendChild(movieEl);
+	})
+}
+// Header End
+
+// Top Movies Start
 GetTopMovies(API_FOR_MOVIES);
 
 function GetTopMovies(url) {
 
 	fetch(url).then(res => res.json()).then(data => {
-		console.log(data.results)
-		return ShowTopMoves(data.results.slice(a,b));
-
+		return ShowTopMoves(data.results.slice(a, b));
 	})
-
 }
-
-
 
 function ShowTopMoves(data) {
 	main.innerHTML = '';
@@ -35,34 +80,37 @@ function ShowTopMoves(data) {
 		const movieEl = document.createElement('div');
 		movieEl.classList.add('MovieImage');
 		movieEl.innerHTML = `
-						<div class="Vote">
+						<div>
 						<h3 style="color:darkblue;">${vote_average}/10</h3>	
 						</div>
-				<img class="MoviesImg"src="${IMG_URL + poster_path}">
-	            	<div class="movie-info">
-	                	<h4 style="color: darkblue">${title}</h4>	             	                                       
-						</div>`
-
+				<img class="MoviesImg"src="${IMG_URL + poster_path}">	           
+	                	<h4 style="color: darkblue">${title}</h4>`
 		main.appendChild(movieEl);
 	})
 }
 
-//
-GetTopTvSeries(API_URL_TV);
+// Top Tv Series Start
+GetTopTvSeries(API_FOR_TV);
+
 function GetTopTvSeries(url) {
 	fetch(url).then(res => res.json()).then(data => {
-		console.log(data.results)
-		return ShowTopTvSeries(data.results.slice(0,4));
+		return ShowTopTvSeries(data.results.slice(0, 4));
 	})
 }
+
 function ShowTopTvSeries(data) {
 	second.innerHTML = '';
 	data.forEach(movie => {
-		const {name, poster_path, vote_average, overview} = movie
+		const {name, poster_path, vote_average} = movie
 		const movieEl = document.createElement('div');
 		movieEl.classList.add('MovieImage');
 		movieEl.innerHTML = `
-					<h3 style="color:darkblue">${vote_average}</h3>	
+					<div class="Tv-info">
+                        <img src="img/Rectangle%209.png">
+                    </div>
+					<h3 style="color:darkblue">${vote_average}</h3>
+						
+						
 				<img class="TvImg"src="${IMG_URL + poster_path}">
 	            	<div class="Tv-info">
 	                	<h3>${name}</h3>	             	                                          
@@ -71,6 +119,7 @@ function ShowTopTvSeries(data) {
 		second.appendChild(movieEl);
 	})
 }
+
 
 
 
